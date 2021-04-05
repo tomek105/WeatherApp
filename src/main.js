7cea2d4eeaff8250603928fd61cb93230373f88c
Tomasz <lisssu14@gmail.com>
2021-04-05 16:46:25 +0200

    Added request handling

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+import {
+    getWheaterByCity
+} from "./apiService.js";
 const viewElement = {};
 
 const getDOMElem = (id) => {
@@ -17,21 +20,31 @@ const connectHTMLElements = () => {
     viewElement.weatherCurrentTemp = getDOMElem('weatherCurrentTemp');
     viewElement.weatherMaxTemp = getDOMElem('weatherMaxTemp');
     viewElement.weatherMinTemp = getDOMElem('weatherMinTemp');
-    
+
     viewElement.returnToSearchBtn = getDOMElem('returnToSearchBtn');
 }
-
 
 const setupListeners = () => {
     viewElement.searchInput.addEventListener('keydown', onEnterSubmit);
     viewElement.searchButton.addEventListener('click', onClickSubmit);
 }
 
-const onEnterSubmit = () => {};
-const onClickSubmit = () => {};
 
 const initializeApp = () => {
     connectHTMLElements();
     setupListeners();
 }
+
+
+const onEnterSubmit = (event) => {
+    if (event.key === "Enter") {
+        let query = viewElement.searchInput.value;
+        getWheaterByCity(query).then(data =>{
+            console.log(data);
+        });
+    }
+};
+const onClickSubmit = () => {};
+
+
 document.addEventListener("DOMContentLoaded", initializeApp)
