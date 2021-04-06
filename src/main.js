de65fe3461d3ab316b0cc262452cb74a1e37f144
Tomasz <lisssu14@gmail.com>
2021-04-06 17:25:08 +0200

    Finished version 1. Added data implementation and visualisation, fixed fading and onClick handler

--- src/main.js
+++ src/main.js
@@ -41,22 +41,40 @@ const onEnterSubmit = (event) => {
     if (event.key === "Enter") {
         fadeInOut();
         let query = viewElement.searchInput.value;
-        getWheaterByCity(query).then(data => {
-            console.log(data);
-            switchView();
-            fadeInOut();
+        getWheaterByCity(query).then(data => {           
+            displayWeatherData(data);
         });
     }
 };
+
 const onClickSubmit = () => {
     fadeInOut();
     let query = viewElement.searchInput.value;
     getWheaterByCity(query).then(data => {
         console.log(data);
-        switchView();
-        fadeInOut();
+        displayWeatherData(data);
     });
 };
+
+const displayWeatherData = data =>{
+    switchView();
+    fadeInOut();
+    console.log(data)
+    const weather = data.consolidated_weather[0];
+
+    viewElement.weatherCity.innerText = data.title;
+    viewElement.weatherIcon.src = `https://www.metaweather.com/static/img/weather/${weather.weather_state_abbr}.svg`;
+    viewElement.weatherIcon.alt = weather.weather_state_name;
+
+    const currTemp = weather.the_temp.toFixed(2);
+    const minTemp = weather.min_temp.toFixed(2);
+    const maxTemp = weather.max_temp.toFixed(2);
+
+    viewElement.weatherCurrentTemp.innerText =`Current temperature is: ${currTemp}°C`
+    viewElement.weatherMaxTemp.innerText = `Max temperature is: ${maxTemp}°C`
+    viewElement.weatherMinTemp.innerText = `Min temperature is: ${minTemp}°C`
+
+}
 
 const fadeInOut = () => {
     if (viewElement.mainContainer.style.opacity === '1' || viewElement.mainContainer.style.opacity === '') {
